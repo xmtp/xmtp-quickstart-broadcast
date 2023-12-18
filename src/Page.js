c0e5bb8535cc3f9411d05cf030d0faf1c91990b3
fabri <fguespe@gmail.com>
2023-12-18 09:07:43 -0300

    16

--- src/Page.js
+++ src/Page.js
@@ -13,17 +13,20 @@ const InboxPage = () => {
   };
 
   return (
-    <Broadcast
-      env="production"
-      walletAddresses={[
-        "0x93E2fc3e99dFb1238eB9e0eF2580EFC5809C7204",
-        "0xdf9A8d961A55e75E1FAEc72037f89251f84ADCc3",
-      ]}
-      placeholderMessage="Enter a broadcast message here"
-      onMessageSuccess={(message) =>
-        console.log("Message sent" + message.content)
-      }
-    />
+    <div style={styles.homePageWrapper}>
+      <h1>Subscribe Button </h1>
+      <Broadcast
+        env="production"
+        walletAddresses={[
+          "0x93E2fc3e99dFb1238eB9e0eF2580EFC5809C7204",
+          "0xdf9A8d961A55e75E1FAEc72037f89251f84ADCc3",
+        ]}
+        placeholderMessage="Enter a broadcast message here"
+        onMessageSuccess={(message) =>
+          console.log("Message sent" + message.content)
+        }
+      />
+    </div>
   );
 };
 
